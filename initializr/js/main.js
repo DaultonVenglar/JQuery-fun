d236d57143324959e9ce8e3b62f5605f6eab61b6
DaultonVenglar <daulton.venglar@utexas.edu>
2016-11-15 19:25:24 -0600

    made some stuff work

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -1,8 +1,21 @@
 function highlight() {
   $('h1').text('Hello!');
-  $('.jumbotron').addClass('highlighted')
+  $('.jumbotron').addClass('highlighted');
+
 }
 function unhighlight() {
-  $('h1').text('Hello, World!')
-  $('.jumbotron').removeClass('highlighted')
+  $('h1').text('Hello, World!');
+  $('.jumbotron').removeClass('highlighted');
 }
+function hide() {
+  $('.jumbotron').fadeOut();
+}
+function show() {
+  $('.jumbotron').slideDown();
+}
+$(document).ready(function() {
+$('h1').on('mouseenter', highlight);
+$('.jumbotron').on('mouseenter', highlight);
+$('h1').on('mouseleave', unhighlight);
+$('.jumbotron').on('mouseleave', unhighlight);
+});
